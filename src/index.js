2e7a41d09765a5130fb1aad59e657d2dae14ad50
Dmitrii <never.forget89@gmail.com>
2020-03-18 19:16:11 +0300

    add redux advanced

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import reducer from './store/reducer';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
 
-const store = createStore(reducer);
+const store = createStore(burgerBuilderReducer);
 
 ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
 
